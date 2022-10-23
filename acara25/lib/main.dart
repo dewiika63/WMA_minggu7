d2423fb123753bf15e814f62b7f89a4e9f8264b3
dewiika63 <dewiika6303@gmail.com>
2022-10-23 20:13:44 +0700

    first commit

---
+++ acara25/lib/main.dart
@@ -0,0 +1,65 @@
+import 'dart:html';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Belajar form flutter",
+    home: BelajarForm(),
+  ));
+}
+
+class BelajarForm extends StatefulWidget {
+  @override
+  _BelajarFormState createState() => _BelajarFormState();
+}
+
+class _BelajarFormState extends State<BelajarForm> {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Belajar Form Flutter"),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: new InputDecoration(
+                    hintText: "Contoh: Dewi Ika Ratnawati",
+                    labelText: "Nama Lengkap",
+                    icon: Icon(Icons.people),
+                    border: OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(5.0)),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Nama tidak boleh kosong';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              // TexField(),
+              RaisedButton(
+                child: Text(
+                  "Submit",
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.blue,
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {}
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ acara28/lib/main.dart
@@ -0,0 +1,82 @@
+import 'dart:html';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Form Register Dewiika",
+    home: BelajarForm(),
+  ));
+}
+
+class BelajarForm extends StatefulWidget {
+  @override
+  _BelajarFormState createState() => _BelajarFormState();
+}
+
+class _BelajarFormState extends State<BelajarForm> {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Form Register Dewiika"),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: new InputDecoration(
+                    hintText: "Contoh: Dewi Ika Ratnawati",
+                    labelText: "Nama Lengkap",
+                    icon: Icon(Icons.people),
+                    border: OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(5.0)),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: new InputDecoration(
+                    labelText: "Alamat",
+                    icon: Icon(Icons.house),
+                    border: OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(5.0)),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  obscureText: true,
+                  decoration: new InputDecoration(
+                    labelText: "Password",
+                    icon: Icon(Icons.security),
+                    border: OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(5.0)),
+                  ),
+                ),
+              ),
+              // TexField(),
+              RaisedButton(
+                child: Text(
+                  "Submit",
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.blue,
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {}
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
